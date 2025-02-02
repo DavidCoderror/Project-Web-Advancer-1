738cd985fe0041108902e63258f98ae98aa2f4bc
DavidCoderror <xbowgaming0@gmail.comoderror>
2025-02-02 18:40:34 -0500

    Etape 4: Creation des Produits

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,72 @@
 import './App.css';
 import axios from 'axios'
+
 import {useEffect, useState} from 'react'
+
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from 'yup'
 
 function App() {
 
 const [listProduits, setListProduits] = useState([])
-
 
 //On va chercher le liste
 useEffect(() => {
   axios.get("http://localhost:3002/produits").then((reponse) => {
     setListProduits(reponse.data)
 
-  }) //Get Request
+  }) //Get 
 }, [])
+
+
+//Verifier que c'est pas Null les entrez des utilisateur
+const verification = Yup.object().shape({
+  Nom: Yup.string().required("Nom est necessaire!!! "),
+  Descriiption: Yup.string().required("Description est necessaire!!! ")
+})
+//Necessaire
+const valeurInitiales = {
+  Nom:"",
+  Descriiption:"",
+}
+//Pour la Soummision du data, dans un array et pas object
+const enSoummision = (data) => {
+  axios.post("http://localhost:3002/produits", data).then((reponse) => {
+    setListProduits((prev) => [...prev, reponse.data]);
+  })
+}
+
 
 //Section HTML
   return (
     <div className="App">
+
+        <Formik 
+        initialValues={valeurInitiales} 
+        onSubmit ={enSoummision} 
+        validationSchema={verification}
+        >
+          <Form>
+            <label>Nom du Produit: </label>
+            <ErrorMessage name ="Nom" component="span"/>
+            <Field id = "inputCreationProduits" name="Nom" placeholder="Nom ICI"/>
+
+            <br/>
+
+            <label>Description du Produit: </label>
+            <ErrorMessage name ="Descriiption" component="span"/>
+            <Field id = "inputCreationProduits" name="Descriiption" placeholder="Description ICI"/>
+
+            <br/>
+
+            <button type="Submit">Creer Produit</button>
+
+          </Form>
+        </Formik>
+
+        <br/> 
+        <br/>
+
         {listProduits.map((valeur, cle) => {
           return (
           <div> 
@@ -25,6 +74,7 @@ useEffect(() => {
                 <div> {valeur.Descriiption} </div>
           </div>
           )//Return Fin
+
         })}
     </div>
   );
